1562626035b462873b9cec19a5ef9af1366e9bb9
EdgarSalasV <edgardre01@gmail.com>
2021-10-15 12:05:30 -0500

    funcionalidad balance general part-2

--- src/components/Investments/GeneralBalance.tsx
+++ src/components/Investments/GeneralBalance.tsx
@@ -20,11 +20,6 @@ export const GeneralBalance: FC<iGeneralBalanceProps> = (
   { investmentList, coinsDatails, coinList }) => {
   const months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   const [metricsByMonth, setMetricsByMonth] = useState<{ [key: string]: iMetricsByMonth }>({})
-  console.log('investmentList', investmentList)
-
-  useEffect(() => {
-    setMetricsFc()
-  }, [])
 
   useEffect(() => {
     let newMetric: iMetricsByMonth = {}
@@ -63,31 +58,38 @@ export const GeneralBalance: FC<iGeneralBalanceProps> = (
   const getMetricsByMonth = () => {
     let metric: iMetricsByMonth = {};
     for (const { coin } of coinList) {
+      console.log('#################################', coin)
       const dateList = investmentList[coin]
-      if (!dateList) return
-
+      if (!dateList) continue
       const dateKeys = Object.keys(dateList)
       dateKeys.map(date => {                                           // key to MONTH
+        const month = new Date(date).getMonth() + 1
+        console.log('dateList '+ coin, dateList)
         const metricTmp = dateList[date].reduce((a: { [key: string]: iMetricsByMonth }, inv) => {
-          const month = new Date(date).getMonth() + 1
+          console.log('///////////////////////////////', coin, inv)
           const coinDetail = coinsDatails.find(({ slug }) => slug === coin)
           const perMonth = coinList.find(({ coin: coinMap }) => coin === coinMap)
 
           const price_usd = coinDetail ? coinDetail.metrics.market_data.price_usd : 0
           const roiMonth = perMonth ? perMonth.roiMonth : 0
+          console.log('metricsByMonth', metricsByMonth)
           if (!a.hasOwnProperty(month)) {
             a = {
-              [month]: {
-                [coin]: {
-                  amount: inv.amount,
-                  costDollar: price_usd,
-                  perMonth: roiMonth,
-                  totalCripto: (price_usd * inv.amount) * roiMonth
+              ...{
+                [month]: {
+                  [coin]: {
+                    amount: inv.amount,
+                    costDollar: price_usd,
+                    perMonth: roiMonth,
+                    totalCripto: (price_usd * inv.amount) * roiMonth
+                  }
                 }
-              }
+              },
+              ...metricsByMonth[month],
             }
           } else {
             a = {
+              ...metricsByMonth,
               ...a,
               ...{
                 [month]: {
@@ -101,34 +103,17 @@ export const GeneralBalance: FC<iGeneralBalanceProps> = (
               }
             }
           }
-
+          console.log('a', a)
           return a
         }, {})
-        setMetricsByMonth({ ...metricsByMonth, ...metricTmp })
+        console.log('metricTmp', metricTmp)
+        setMetricsByMonth({ [month]: { ...metricsByMonth[month], ...metricTmp[month] } })
       })
     }
 
   }
 
-  const setMetricsFc = () => {
-    // const newMetrics: { [key: string]: iMetricsByMonth } = coinList.reduce((a, { coin }) => {
-    //   a = {
-    //     ...a,
-    //     ...{
-    //       [(new Date()).toLocaleDateString()]: {
-    //         [coin]: {
-    //           costDollar: 0,
-    //           totalCripto: 0,
-    //           perMonth: 0,
-    //           investment: 0
-    //         }
-    //       }
-    //     }
-    //   }
-    //   return a
-    // }, {})
-    // setMetricsByMonth(newMetrics)
-  }
+  console.log("metrics", metricsByMonth)
 
   const BalanceTd = (nameCoin: string) => {
     // months.map((month, i) => {
@@ -142,6 +127,7 @@ export const GeneralBalance: FC<iGeneralBalanceProps> = (
     // })
     return <th key={nameCoin}>X</th>
   }
+
   return <>
     <div className="GeneralBalance">
       {investmentList &&
